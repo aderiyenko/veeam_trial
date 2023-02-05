a8a19e4c9bf11a07325cbbc28a14f98fba50eb6a
Alexey Deriyenko <oderiyenko@wayfair.com>
2023-02-05 13:05:30 +0100

    adding initial file structure

---
+++ ExchangeRates.php
@@ -0,0 +1,19 @@
+<?php
+namespace MediaWiki\Extension\CurrencyExchangeRates;
+
+use MediaWiki\MediaWikiServices;
+use SpecialPage;
+
+class ExchangeRates extends SpecialPage {
+    public function __construct() {
+        parent::__construct('ExchangeRates', '', false);
+    }
+
+    public function execute($subPage) {
+        $output = $this->getOutput();
+        $output->addModuleStyles("exchange.rates");
+        $output->addModules("exchange.rates");
+
+        $output->addHTML('<div id="exchange_rates"></div>');
+    }
+}
